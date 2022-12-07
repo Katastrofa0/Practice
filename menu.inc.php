4e61172a32ed0862e3244680812ab84bbb7f7707
Katastrofa0 <119258885+Katastrofa0@users.noreply.github.com>
2022-12-07 23:09:32 +0300

    Delete menu.inc.php

--- menu.inc.php
+++
@@ -1,5 +0,0 @@
-<nav align="center">
-    <a href="#su" style="color: whitesmoke; padding: 10px; font-size: 100%">'ABOUT ME</a>
-    <a href="#bu" style="color: whitesmoke; padding: 10px; font-size: 100%">MY SKILLS</a>
-    <a href="#gu" style="color: whitesmoke; padding: 10px; font-size: 100%">CONTACT ME'</></a>
-</nav>
